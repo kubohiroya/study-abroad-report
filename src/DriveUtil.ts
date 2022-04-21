80cd87fde8a61234dce69c97b0959473ab4395db
Hiroya Kubo <hiroya@cuc.ac.jp>
2022-04-22 01:42:21 +0900

    bind the Apps Script project to a Google Cloud Platform(GCP) to use Drive API directly to gain better performance

--- src/DriveUtil.ts
+++ src/DriveUtil.ts
@@ -1,21 +1,14 @@
+type File = GoogleAppsScript.Drive.Schema.File;
+
 export class DriveUtil {
 
-  static getFile(url: string): { contentType: string, thumbnailData: string } {
+  static getFileIdByUrl(url: string): string {
     const [base, fileId] = url.split(/=/);
-    try {
-      const file = DriveApp.getFileById(fileId);
-      const contentType = file.getBlob().getContentType();
-      const thumbnailBlob = file.getThumbnail();
-      const thumbnailContentType = thumbnailBlob.getContentType();
-      const thumbnailEncodedBytes = Utilities.base64Encode(thumbnailBlob.getBytes());
-      return {
-        contentType,
-        thumbnailData: 'data:' + thumbnailContentType + ';base64,' + thumbnailEncodedBytes
-      };
-    } catch (error) {
-      Logger.log("Invalid URL " + error + " = " + url);
-      throw error;
-    }
+    return fileId;
+  }
+
+  static getFileByFileId(fileId: string): File | undefined {
+    return Drive.Files?.get(fileId);
   }
 
   static getDriveFileHTML(urlStrArray: string): Array<string> {
@@ -28,13 +21,20 @@ export class DriveUtil {
         return "";
       }
       let out = `<a target="_blank" href="${url}">`;
-      const file = DriveUtil.getFile(url);
-      if (file.contentType.startsWith("image")) {
-        out += `<img alt="image" src="${file.thumbnailData}">`;
-      } else if (file.contentType.startsWith("video")) {
-        out += `<div class="video"><img alt="video" src="${file.thumbnailData}"><div class="overlay"><span class="material-icons">play_circle</span></div></div>`;
-      } else {
-        out += `(${file.contentType} file)`
+
+      const fileId = DriveUtil.getFileIdByUrl(url);
+      const file = DriveUtil.getFileByFileId(fileId);
+      const mimeType = file?.mimeType;
+      const thumbnailSrc = file?.thumbnailLink;
+
+      if (mimeType) {
+        if (mimeType.startsWith("image")) {
+          out += `<img alt="image" src="${thumbnailSrc}">`;
+        } else if (mimeType.startsWith("video")) {
+          out += `<div class="video"><img alt="video" src="${thumbnailSrc}"><div class="overlay"><span class="material-icons">play_circle</span></div></div>`;
+        } else {
+          out += `(${mimeType} file)`
+        }
       }
       out += "</a>";
       return out;
